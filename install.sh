d5c683764e05104cd16f534b77d735fff43e8478
sena <sena@lichtlabs.org>
2025-01-21 03:44:48 +0700

    initial changes

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Generating The Hardware Configuration"
+sudo nixos-generate-config --show-hardware-config > ./hosts/$hostName/hardware.nix
+
+echo "-----"
+
+echo "Setting Required Nix Settings Then Going To Install"
+NIX_CONFIG="experimental-features = nix-command flakes"
+
+echo "-----"
+
+sudo nixos-rebuild switch --flake ~/zaneyos/#${hostName}
