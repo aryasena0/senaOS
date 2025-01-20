9dfffe5ae9abd4e87dbf5b55d42476a385404e0f
sena <sena@lichtlabs.org>
2025-01-21 04:01:34 +0700

    fix(install.sh): update hardware configuration generation to use fixed hostName 'lainix' for consistency fix(install.sh): modify nixos-rebuild command to reference fixed hostName 'lainix' for clarity and reliability

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Generating The Hardware Configuration"
-sudo nixos-generate-config --show-hardware-config > ./hosts/$hostName/hardware.nix
+sudo nixos-generate-config --show-hardware-config > ./hosts/lainix/hardware.nix
 
 echo "-----"
 
@@ -9,4 +9,4 @@ NIX_CONFIG="experimental-features = nix-command flakes"
 
 echo "-----"
 
-sudo nixos-rebuild switch --flake ~/zaneyos/#${hostName}
+sudo nixos-rebuild switch --flake ~/zaneyos/#lainix
